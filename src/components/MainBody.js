826f5c518f031938a8237478796dd239c8de3482
yzhou2 <samandjesslovepie@gmail.com>
2017-09-20 21:14:01 -0600

    world dishes added

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -32,6 +32,45 @@ export default class MainBody extends Component {
   return (
     <div className='body_container'>
       <TopBar handleChange={this.handleChange} input={this.state.input} searchFoods={this.searchFoods}/>
+
+      <div className="body_inner">
+        <div className='body_title'>
+        <p className="subtitle">World Cuisines</p>
+        </div>
+
+      <div className="worldwraper">
+        <div className="country">
+          <div className="countrypic"><img src='https://i.imgur.com/vrgIjOF.png' /></div>
+          <div className="countryName">Mexican</div>
+        </div>
+        <div className="country">
+          <div className="countrypic"><img src='https://i.imgur.com/Te6WGAy.jpg' /></div>
+          <div className="countryName">American</div>
+        </div>
+        <div className="country">
+          <div className="countrypic"><img src='https://i.imgur.com/XbkB3Mc.jpg' /></div>
+          <div className="countryName">Chinese</div>
+        </div>
+        <div className="country">
+          <div className="countrypic"><img src='https://i.imgur.com/fhyxGvr.png' /></div>
+          <div className="countryName">Japanese</div>
+        </div>
+        <div className="country">
+          <div className="countrypic"><img src='https://i.imgur.com/R2Jsk0X.jpg' /></div>
+          <div className="countryName">African</div>
+        </div>
+        <div className="country">
+          <div className="countrypic"><img src='https://i.imgur.com/4IRSgLX.png' /></div>
+          <div className="countryName">Greek</div>
+        </div>
+      </div>
+
+
+
+      </div>
+
+
+
       <div className="body_inner">
         <div className='body_title'>
         <p className="subtitle">Quick & Easy</p>
