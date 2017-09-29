65f9c9027580e3257b86eb4aaa3eee36755c3fee
yzhou2 <samandjesslovepie@gmail.com>
2017-09-29 16:36:22 -0600

    rugs section done

--- src/components/Kitchenware.js
+++ src/components/Kitchenware.js
@@ -45,18 +45,18 @@ export default class MainBody extends Component {
       </div>
 
         <div className="sectionBox">
-          <div className="leftbox">
-            <div className="textBox">
-              <div className="textTitle">Essentials</div>
-              <div className="textBody">Our essential collection emphasize convinience, fun and innovative craftsmanship for your dream kitchen.</div>
+          <div className="leftbox leftAltBox">
+            <div className="textBox tbAltBox">
+              <div className="textTitle">Rugs</div>
+              <div className="textBody">From kitchen rugs to oven gloves, Foodies Goodies committed to bring you the best experience.</div>
               <div className="viewall">VIEW ALL</div>
             </div>
 
-            <div className="insertLine"></div>
+            <div className="insertLine altLine"></div>
           </div>
 
           <div className="rightbox">
-            <img src='https://i.imgur.com/CzHdUxQ.jpg' />
+            <img src='https://i.imgur.com/f9QEyLC.jpg' />
           </div>
       </div>
 
