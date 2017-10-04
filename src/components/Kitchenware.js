124a0e029ca381c417783ebe753a8ae3d79d7c5e
yzhou2 <samandjesslovepie@gmail.com>
2017-10-03 23:29:02 -0600

    rebuilt pot and pans section

--- src/components/Kitchenware.js
+++ src/components/Kitchenware.js
@@ -17,6 +17,45 @@ export default class MainBody extends Component {
 
 
   render() {
+
+    // <div className="sectionBox">
+    // <div className="rightbox pot">
+    //   <img src="https://preview.ibb.co/mdNp6G/potkit.jpg" />
+    // </div>
+    //
+    // <div className="leftbox potText">
+    //   <div className="insertLine"></div>
+    //   <div className="textBox potTextContent">
+    //     <div className="textTitle">Pots & Pans</div>
+    //     <div className="textBody">Our essential collection emphasize convinience, fun and innovative craftsmanship for your dream kitchen.</div>
+    //     <div className="viewall">VIEW ALL</div>
+    //   </div>
+    // </div>
+    // </div>
+
+
+
+
+
+
+
+    //
+    // <div className="sectionBox">
+    // <div className="rightbox">
+    //   <img src="https://preview.ibb.co/e8HE6G/potkitchen2.jpg" alt="potkitchen2" border="0"/>
+    // </div>
+    //
+    // <div className="leftbox">
+    //   <div className="insertLine"></div>
+    //   <div className="textBox">
+    //     <div className="textTitle">Essentials</div>
+    //     <div className="textBody">Our essential collection emphasize convinience, fun and innovative craftsmanship for your dream kitchen.</div>
+    //     <div className="viewall">VIEW ALL</div>
+    //   </div>
+    // </div>
+    // </div>
+
+
   return (
     <div>
       <div className="sectionBox">
@@ -60,9 +99,10 @@ export default class MainBody extends Component {
           </div>
       </div>
 
+
       <div className="sectionBox">
       <div className="rightbox pot">
-        <img src="https://preview.ibb.co/mdNp6G/potkit.jpg" />
+        <img src="https://preview.ibb.co/fKDnRG/chard_616711_1920.jpg" alt="chard_616711_1920" border="0"/>
       </div>
 
       <div className="leftbox potText">
@@ -74,6 +114,7 @@ export default class MainBody extends Component {
         </div>
       </div>
       </div>
+
 
 
     </div>
