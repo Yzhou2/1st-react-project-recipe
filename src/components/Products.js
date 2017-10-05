e1da6b26e80a8aff5abd33f28ee5c1c8864f9741
yzhou2 <samandjesslovepie@gmail.com>
2017-10-05 13:52:46 -0600

    single product done

--- src/components/Products.js
+++ src/components/Products.js
@@ -26,6 +26,70 @@ export default class Products extends Component {
         <img src="https://preview.ibb.co/jO57dw/Screen_Shot_2017_10_05_at_12_56_13_PM.png" />
         <div className="bannerTitle">Convinient, Elegant, Fun</div>
       </div>
+
+      <div className="storeMenu">
+        <div>Essentials</div>
+        <div>Rugs</div>
+        <div>Pots & Pans</div>
+      </div>
+
+      <div className="productStore">
+        <div className="storeFrame">
+          <div className="singleProduct">
+            <div className="producPic">
+              <img src="http://cdn.shopify.com/s/files/1/0203/1620/products/bambu-home-kitchen-basics-22720436621_grande.jpg?v=1497643746" />
+            </div>
+            <div className="productInfo">
+              <div className="price"><span>$ 20</span>.00</div>
+              <div className="_atc">add to cart</div>
+            </div>
+          </div>
+
+          <div className="singleProduct">
+            <div className="producPic"></div>
+            <div className="productInfo"></div>
+          </div>
+
+          <div className="singleProduct">
+            <div className="producPic"></div>
+            <div className="productInfo"></div>
+          </div>
+
+          <div className="singleProduct">
+            <div className="producPic"></div>
+            <div className="productInfo"></div>
+          </div>
+
+          <div className="singleProduct">
+            <div className="producPic"></div>
+            <div className="productInfo"></div>
+          </div>
+
+
+          <div className="singleProduct">
+            <div className="producPic"></div>
+            <div className="productInfo"></div>
+          </div>
+
+
+          <div className="singleProduct">
+            <div className="producPic"></div>
+            <div className="productInfo"></div>
+          </div>
+
+          <div className="singleProduct">
+            <div className="producPic"></div>
+            <div className="productInfo"></div>
+          </div>
+
+          <div className="singleProduct">
+            <div className="producPic"></div>
+            <div className="productInfo"></div>
+          </div>
+
+
+        </div>
+      </div>
     </div>
     )
   }
