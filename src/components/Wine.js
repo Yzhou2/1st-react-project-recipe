40e9ad9edf501de59307a2b2a9d7a006b86fc4ed
yzhou2 <samandjesslovepie@gmail.com>
2017-10-11 13:57:39 -0600

    wine store layout

--- src/components/Wine.js
+++ src/components/Wine.js
@@ -15,25 +15,39 @@ class Wine extends Component {
 
 render() {
   var settings = {
-        dots: true,
+        arrows: true,
+        className: 'slide',
         speed: 1000,
         fade: true,
         // slidesToShow: 1,
         // slidesToScroll: 1
       };
 
+      // <Slider {...settings}>
+      //     <div><h3>11111111111</h3></div>
+      //     <div><h3>22222222222</h3></div>
+      //     <div><h3>33333333333</h3></div>
+      //     <div><h3>44444444444</h3></div>
+      //     <div><h3>55555555555</h3></div>
+      //     <div><h3>66666666666</h3></div>
+      //   </Slider>
 
 
   return (
     <div className="wineStore">
-      <Slider {...settings}>
-          <div><h3>11111111111</h3></div>
-          <div><h3>22222222222</h3></div>
-          <div><h3>33333333333</h3></div>
-          <div><h3>44444444444</h3></div>
-          <div><h3>55555555555</h3></div>
-          <div><h3>66666666666</h3></div>
-        </Slider>
+        <div className="storeBody"></div>
+        <div className="storeBottom">
+          <div className="wineWrapper">
+            <div className="innerWrapper">
+              <div className="wineDetails"></div>
+              <img src="http://www.winemadeeasy.com/media/catalog/product/cache/1/image/300x480/0dc2d03fe217f8c83829496872af24a0/c/h/ch_coulon_mv_750.png" className="inactiveWine"/>
+              <img src="http://www.winedeals.com/media/catalog/product/cache/1/image/300x480/0dc2d03fe217f8c83829496872af24a0/c/h/ch_famey_trad_malbec_11_750.png" className="activeWine" />
+              <img src="https://www.foleyfoodandwinesociety.com/assets/images/products/pictures/1312-5601.png" className="inactiveWine"/>
+            </div>
+            <img src="http://www.winemadeeasy.com/media/catalog/product/cache/1/image/300x480/0dc2d03fe217f8c83829496872af24a0/s/m/smith_son_cab_12_750.png" className="inactiveWine"/>
+            <img src="http://pngimg.com/uploads/wine/wine_PNG9462.png" className="inactiveWine"/>
+          </div>
+        </div>
     </div>
     )
   }
