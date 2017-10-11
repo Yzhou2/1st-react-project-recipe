858a842a1cc3c50e38f42d87ffd03b22a1894eae
yzhou2 <samandjesslovepie@gmail.com>
2017-10-11 14:15:49 -0600

    basic layout wine store done

--- src/components/Wine.js
+++ src/components/Wine.js
@@ -24,7 +24,27 @@ render() {
       };
 
       // <Slider {...settings}>
-      //     <div><h3>11111111111</h3></div>
+      // <div className="wineStore">
+      //     <div className="storeBody"></div>
+      //     <div className="storeBottom">
+      //       <div className="wineWrapper">
+      //         <div className="innerWrapper">
+      //           <div className="wineDetails">
+      //             <div className="wineTitle">PAMARY</div>
+      //             <div className="winePrice">$ 125.99 </div>
+      //             <div className="wineDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+      //             <div className="wineBuy"><span>Buy Now</span></div>
+      //
+      //           </div>
+      //           <img src="http://www.winemadeeasy.com/media/catalog/product/cache/1/image/300x480/0dc2d03fe217f8c83829496872af24a0/c/h/ch_coulon_mv_750.png" className="inactiveWine"/>
+      //           <img src="http://www.winedeals.com/media/catalog/product/cache/1/image/300x480/0dc2d03fe217f8c83829496872af24a0/c/h/ch_famey_trad_malbec_11_750.png" className="activeWine" />
+      //           <img src="https://www.foleyfoodandwinesociety.com/assets/images/products/pictures/1312-5601.png" className="inactiveWine"/>
+      //         </div>
+      //         <img src="http://www.winemadeeasy.com/media/catalog/product/cache/1/image/300x480/0dc2d03fe217f8c83829496872af24a0/s/m/smith_son_cab_12_750.png" className="inactiveWine"/>
+      //         <img src="http://pngimg.com/uploads/wine/wine_PNG9462.png" className="inactiveWine"/>
+      //       </div>
+      //     </div>
+      // </div>
       //     <div><h3>22222222222</h3></div>
       //     <div><h3>33333333333</h3></div>
       //     <div><h3>44444444444</h3></div>
@@ -39,7 +59,13 @@ render() {
         <div className="storeBottom">
           <div className="wineWrapper">
             <div className="innerWrapper">
-              <div className="wineDetails"></div>
+              <div className="wineDetails">
+                <div className="wineTitle">PAMARY</div>
+                <div className="winePrice">$ 125.99 </div>
+                <div className="wineDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+                <div className="wineBuy"><span>Buy Now</span></div>
+
+              </div>
               <img src="http://www.winemadeeasy.com/media/catalog/product/cache/1/image/300x480/0dc2d03fe217f8c83829496872af24a0/c/h/ch_coulon_mv_750.png" className="inactiveWine"/>
               <img src="http://www.winedeals.com/media/catalog/product/cache/1/image/300x480/0dc2d03fe217f8c83829496872af24a0/c/h/ch_famey_trad_malbec_11_750.png" className="activeWine" />
               <img src="https://www.foleyfoodandwinesociety.com/assets/images/products/pictures/1312-5601.png" className="inactiveWine"/>
